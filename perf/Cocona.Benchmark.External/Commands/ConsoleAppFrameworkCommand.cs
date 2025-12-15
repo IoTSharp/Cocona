0b0d08dceabf552229354b471bec52788de5c21b
maikebing <mysticboy@live.com>
2025-12-16 03:34:20 +0800

    upgrade ConsoleAppFramework  to 5.7.X

--- perf/Cocona.Benchmark.External/Commands/ConsoleAppFrameworkCommand.cs
+++ perf/Cocona.Benchmark.External/Commands/ConsoleAppFrameworkCommand.cs
@@ -1,16 +1,41 @@
 using ConsoleAppFramework;
 
 namespace Cocona.Benchmark.External.Commands;
+//https://github.com/Cysharp/ConsoleAppFramework/blob/1fc5a9a4a558f0415175df414570c5ce8844b843/sandbox/CliFrameworkBenchmark/Commands/ConsoleAppFrameworkCommand.cs#L4
+//public class ConsoleAppFrameworkCommand : ConsoleAppBase
+//{
+//    public void Execute(
+//        [global::ConsoleAppFramework.Option("s")]
+//        string? str,
+//        [global::ConsoleAppFramework.Option("i")]
+//        int intOption,
+//        [global::ConsoleAppFramework.Option("b")]
+//        bool boolOption)
+//    {
+//    }
+//}
 
-public class ConsoleAppFrameworkCommand : ConsoleAppBase
+public class ConsoleAppFrameworkCommand
 {
-    public void Execute(
-        [global::ConsoleAppFramework.Option("s")]
-        string? str,
-        [global::ConsoleAppFramework.Option("i")]
-        int intOption,
-        [global::ConsoleAppFramework.Option("b")]
-        bool boolOption)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="str">-s</param>
+    /// <param name="intOption">-i</param>
+    /// <param name="boolOption">-b</param>
+    public static void Execute(string? str, int intOption, bool boolOption)
     {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="str">-s</param>
+    /// <param name="intOption">-i</param>
+    /// <param name="boolOption">-b</param>
+    public static Task ExecuteWithCancellationToken(string? str, int intOption, bool boolOption, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
     }
 }
